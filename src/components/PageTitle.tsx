2154907b8e25df751c5bd28e59dbff3d2c3d8759
huylee99 <huy.le7@pcc.edu>
2022-06-14 20:57:12 -0700

    poll creation form ui

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,6 @@
 const PageTitle: React.FC<{ title: string }> = ({ title }) => {
   return (
-    <h1 className="text-5xl bg-clip-text text-center font-extrabold text-transparent bg-gradient-to-r from-pink-400 to-red-500 mb-4">
+    <h1 className="text-5xl bg-clip-text text-center font-extrabold text-transparent bg-gradient-to-r from-pink-400 to-red-500 mb-8">
       {title}
     </h1>
   );
--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,6 +1,6 @@
 import type { createPollType } from "src/shared/createPollValidator";
 
-import { useRef } from "react";
+import { XIcon } from "@heroicons/react/outline";
 import { trpc } from "@utils/trpc";
 import { useForm, useFieldArray } from "react-hook-form";
 import { NextPage } from "next";
@@ -43,33 +43,61 @@ const QuestionCreator: NextPage = () => {
     <div className="container">
       <main>
         <PageTitle title="Create Poll" />
-        <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="text" {...register("question")} placeholder="Question?" />
-            {fields.map((field, index) => {
-              return (
-                <div key={field.id}>
-                  <section className={"section"} key={field.id}>
+        <div className="w-full bg-slate-800 border-t-4 border-pink-600 rounded-md p-8">
+          <form onSubmit={handleSubmit(onSubmit)} className="w-full antialiased">
+            <div className="mb-4">
+              <label htmlFor="question" className="inline-block text-slate-300 font-medium mb-2">
+                Poll Question
+              </label>
+              <input
+                type="text"
+                {...register("question")}
+                placeholder="What is my name?"
+                className="w-full p-3 bg-slate-700 text-slate-400 rounded-md focus:ring-1 focus:ring-pink-500 focus:outline-none"
+              />
+            </div>
+
+            <label className="inline-block text-slate-300 font-medium mb-2">Options</label>
+            <div className="flex flex-col -my-1 mb-4">
+              {fields.map((field, index) => {
+                return (
+                  <div key={field.id} className="flex items-center relative my-1">
                     <input
-                      placeholder="Option"
+                      placeholder={`Option ${index + 1}`}
                       {...register(`options.${index}.content`, {
                         required: true,
                       })}
-                      className={errors?.options && errors?.options[index]?.content ? "text-red-600" : ""}
+                      className="w-full py-3 px-4 pr-10 bg-slate-700 text-slate-400 rounded-md focus:ring-1 focus:ring-pink-500 focus:outline-none"
                     />
 
-                    <button type="button" onClick={() => remove(index)}>
-                      DELETE
-                    </button>
-                  </section>
-                </div>
-              );
-            })}
+                    <XIcon
+                      onClick={() => remove(index)}
+                      className="ml-auto absolute right-2 w-6 h-6 text-slate-400"
+                      role="button"
+                    />
+                  </div>
+                );
+              })}
+            </div>
 
-            <button type="button" onClick={() => append({ content: "" })}>
-              append
+            <button
+              type="button"
+              onClick={() => append({ content: "" })}
+              className="py-2 px-4 bg-slate-700 font-medium text-slate-300 rounded-md hover:bg-slate-600"
+            >
+              Add option
             </button>
-            <button type="submit">Submit</button>
+
+            <div className="my-4 h-[0.5px] bg-slate-600"></div>
+
+            <div className="text-right">
+              <button
+                type="submit"
+                className="py-2 px-4 bg-pink-700 font-medium text-slate-300 rounded-md hover:bg-pink-600"
+              >
+                Create poll
+              </button>
+            </div>
           </form>
         </div>
       </main>
