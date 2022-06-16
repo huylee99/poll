c36c028fcbb256625e6e34d0de1e5bda80b6e764
huylee99 <huy.le7@pcc.edu>
2022-06-16 07:11:56 -0700

    success dialog, share dialog

--- src/components/PollContent.tsx
+++ src/components/PollContent.tsx
@@ -1,15 +1,26 @@
 import type { createVoteFieldType } from "@shared/createVoteValidator";
-import { ChartBarIcon, ShareIcon } from "@heroicons/react/solid";
+
+import { useState } from "react";
+import Link from "next/link";
+import { ChartBarIcon, ShareIcon, CheckIcon } from "@heroicons/react/solid";
 import { trpc } from "@utils/trpc";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/link";
 import { createVoteFieldValue } from "@shared/createVoteValidator";
+import PollDialog from "./Dialog";
+import SharePoll from "./SharePoll";
 
 const PollContent: React.FC<{ id: string }> = ({ id }) => {
-  const { data, isLoading } = trpc.useQuery(["poll.get-by-id", { id }]);
+  const [isOpen, setIsOpen] = useState(false);
+  const { data, isLoading: queryLoading } = trpc.useQuery(["poll.get-by-id", { id }]);
+  const client = trpc.useContext();
 
-  const { mutate } = trpc.useMutation(["vote.create"]);
+  const { mutate, isLoading: mutationLoading } = trpc.useMutation(["vote.create"], {
+    onSuccess: () => {
+      client.invalidateQueries(["poll.get-by-id"]);
+      setIsOpen(true);
+    },
+  });
 
   const { register, handleSubmit } = useForm<createVoteFieldType>({ resolver: zodResolver(createVoteFieldValue) });
 
@@ -17,11 +28,11 @@ const PollContent: React.FC<{ id: string }> = ({ id }) => {
     mutate({ choice: parseInt(data.choice), pollId: id });
   };
 
-  if (!data && !isLoading) {
+  if (!data && !queryLoading) {
     return <div>Question not found</div>;
   }
 
-  if (isLoading) {
+  if (queryLoading) {
     return <div>Loading...</div>;
   }
 
@@ -46,7 +57,7 @@ const PollContent: React.FC<{ id: string }> = ({ id }) => {
                     type="radio"
                     value={`${index}`}
                     className="w-5 h-5 inline-block text-pink-600 bg-slate-500 focus:ring-0 focus:ring-offset-0 focus:outline-none cursor-pointer mr-3 mt-[0.5px]"
-                    disabled={data.isVoted}
+                    disabled={data.isVoted || mutationLoading}
                     defaultChecked={data.myVote?.choice === index}
                   />
                   {option.content}
@@ -54,7 +65,6 @@ const PollContent: React.FC<{ id: string }> = ({ id }) => {
               );
             })}
           </div>
-
           <div className="flex items-center -mx-2">
             <div className="flex-1 mx-2">
               <button
@@ -62,7 +72,7 @@ const PollContent: React.FC<{ id: string }> = ({ id }) => {
                 className="text-center w-full font-medium bg-pink-800 text-slate-200 py-3 rounded-md text-sm hover:bg-pink-700 transition-all disabled:bg-opacity-80 disabled:pointer-events-none"
                 disabled={data.isVoted}
               >
-                {data.isVoted ? "You already voted on this poll." : "Vote"}
+                {data.isVoted ? "You already voted on this poll." : mutationLoading ? "Voting..." : "Vote"}
               </button>
             </div>
             <div className="flex-1 flex items-center">
@@ -74,17 +84,38 @@ const PollContent: React.FC<{ id: string }> = ({ id }) => {
                 </Link>
               </div>
               <div className="flex-1 mx-2">
-                <button
-                  type="button"
-                  className="w-full text-sm rounded-md flex items-center justify-center bg-slate-700 py-3 text-slate-200 hover:bg-slate-600 transition-all"
-                >
-                  <ShareIcon className="w-5 h-5 text-slate-300 mr-2" /> Share
-                </button>
+                <SharePoll shareUrl="http://localhost:3000/poll/cl4fl56cv0000n4vas7wvmjns" />
               </div>
             </div>
           </div>
         </form>
       </div>
+      <PollDialog isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div className="text-center">
+          <CheckIcon className="h-8 w-8 text-green-600 mb-4 mx-auto" />
+          <h3 className="text-lg text-slate-200 mb-2">Vote successful</h3>
+          <p className="text-slate-500 mb-4 text-sm">
+            Thanks for voting on this poll. View the results or share this poll with others!
+          </p>
+          <div className="flex-1 flex items-center">
+            <div className="flex-1 mx-2">
+              <Link href={`/poll/${id}/results`} passHref>
+                <a className="w-full text-sm flex rounded-md items-center justify-center bg-slate-700 py-3 text-slate-200 hover:bg-slate-600 transition-all">
+                  <ChartBarIcon className="w-5 h-5 text-slate-300 mr-2" /> Results
+                </a>
+              </Link>
+            </div>
+            <div className="flex-1 mx-2">
+              <button
+                type="button"
+                className="w-full text-sm rounded-md flex items-center justify-center bg-slate-700 py-3 text-slate-200 hover:bg-slate-600 transition-all"
+              >
+                <ShareIcon className="w-5 h-5 text-slate-300 mr-2" /> Share
+              </button>
+            </div>
+          </div>
+        </div>
+      </PollDialog>
     </>
   );
 };
---
+++ src/components/SharePoll.tsx
@@ -0,0 +1,68 @@
+import { MouseEventHandler, useState } from "react";
+import copy from "copy-to-clipboard";
+import { ShareIcon, ClipboardCopyIcon } from "@heroicons/react/solid";
+import PollDialog from "./Dialog";
+
+type SharePollProps = {
+  shareUrl: string;
+};
+
+const SharePoll = ({ shareUrl }: SharePollProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy: MouseEventHandler<HTMLButtonElement> = () => {
+    copy(shareUrl);
+    setCopied(true);
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="w-full text-sm rounded-md flex items-center justify-center bg-slate-700 py-3 text-slate-200 hover:bg-slate-600 transition-all"
+        onClick={() => setIsOpen(true)}
+      >
+        <ShareIcon className="w-5 h-5 text-slate-300 mr-2" /> Share
+      </button>
+
+      <PollDialog isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <div>
+          <h3 className="text-lg text-slate-200 mb-2">Invite via link</h3>
+          <p className="text-slate-500 mb-4 text-sm">Use this link to invite participants directly.</p>
+          <div className="flex items-stretch rounded-md overflow-hidden">
+            <div className="flex-grow">
+              <label className="sr-only" htmlFor="invite-link">
+                Invite link
+              </label>
+              <input
+                defaultValue={shareUrl}
+                id="invite-link"
+                type="text"
+                readOnly
+                className="bg-slate-700 outline-none border-none w-full text-sm text-slate-400"
+              />
+            </div>
+            <button
+              onClick={handleCopy}
+              type="button"
+              className="ml-auto inline-flex text-sm font-medium text-slate-300 items-center bg-slate-600 px-6 hover:bg-slate-500 transition-all"
+            >
+              <ClipboardCopyIcon className="w-5 h-5 text-slate-300 mr-1" />
+              {copied ? "Copied" : "Copy"}
+            </button>
+          </div>
+          <div className="h-[0.5px] my-4 bg-slate-600"></div>
+          <button
+            type="button"
+            className="w-full text-center bg-slate-600 text-sm text-slate-300 font-medium py-2 rounded-md hover:bg-slate-700 transition-all"
+          >
+            Close
+          </button>
+        </div>
+      </PollDialog>
+    </>
+  );
+};
+
+export default SharePoll;
